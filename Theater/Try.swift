bcb95e36f4073493091e3b22720a93fad50eba5e
darioalessandro <darioalessandrolencina@gmail.com>
2015-09-26 23:31:50 -0400

    changed visibility of isFailure and isSuccess accessors

--- Theater/Try.swift
+++ Theater/Try.swift
@@ -12,11 +12,11 @@ public class Try<T> {
 
     /** Returns `true` if the `Try` is a `Failure`, `false` otherwise.
     */
-    func isFailure() -> Bool {return false}
+    public func isFailure() -> Bool {return false}
     
     /** Returns `true` if the `Try` is a `Success`, `false` otherwise.
     */
-    func isSuccess() -> Bool {return false}
+    public func isSuccess() -> Bool {return false}
 
     
     /** Returns the value from this `Success` or throws the exception if this is a `Failure`.
@@ -30,23 +30,14 @@ public class Try<T> {
 
 public class Success<T> : Try<T> {
     
-    let value : T
+    private let value : T
     
-    public init(value : T) {
-        self.value = value
-    }
+    public init(value : T) { self.value = value}
     
-    /** Returns `true` if the `Try` is a `Failure`, `false` otherwise.
-    */
-    override func isFailure() -> Bool {return false}
+    override public func isFailure() -> Bool {return false}
     
-    /** Returns `true` if the `Try` is a `Success`, `false` otherwise.
-    */
-    override func isSuccess() -> Bool { return true}
+    override public func isSuccess() -> Bool { return true}
     
-    
-    /** Returns the value from this `Success` or throws the exception if this is a `Failure`.
-    */
     override func get() -> T {return self.value}
     
     override public func description() -> String {
@@ -58,22 +49,12 @@ public class Failure<T> : Try<T> {
     
     public let exception : NSError
     
+    public init(exception : NSError) {self.exception = exception}
+
+    override public func isFailure() -> Bool {return true}
     
-    public init(exception : NSError) {
-        self.exception = exception
-    }
+    override public func isSuccess() -> Bool { return false}
     
-    /** Returns `true` if the `Try` is a `Failure`, `false` otherwise.
-    */
-    override func isFailure() -> Bool {return true}
-    
-    /** Returns `true` if the `Try` is a `Success`, `false` otherwise.
-    */
-    override func isSuccess() -> Bool { return false}
-    
-    
-    /** Returns the value from this `Success` or throws the exception if this is a `Failure`.
-    */
     override func get() -> T {
         NSException.raise(self.exception.description, format: "", arguments: getVaList([""]))
         return NSObject() as! T
