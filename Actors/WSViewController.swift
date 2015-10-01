c9ebe6efa2c85c435d3dc7f27810102efcc9d3c2
darioalessandro <darioalessandrolencina@gmail.com>
2015-09-30 22:44:13 -0400

    Finished web socket example

--- Actors/WSViewController.swift
+++ Actors/WSViewController.swift
@@ -9,13 +9,43 @@
 import UIKit
 import Theater
 
-public class WSRViewControler : Actor {
+public class SetWSController: Message {
+    
+    let ctrl : WSViewController
+    
+    init(ctrl : WSViewController) {
+        self.ctrl = ctrl
+        super.init(sender : Optional.None)
+    }
+}
+
+public class WSRViewController : Actor, UITableViewDataSource, UITableViewDelegate  {
     
     let wsClient : ActorRef
+    
+    weak var ctrl : Optional<WSViewController> = Optional.None
+    
+    var receivedMessages : [(String, NSDate)] = [(String, NSDate)]()
     
     public required init(context: ActorSystem, ref: ActorRef) {
         self.wsClient = context.actorOf(WebSocketClient)
         super.init(context: context, ref: ref)
+    }
+    
+    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.receivedMessages.count
+    }
+    
+    public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("device")!
+        let s : (String, NSDate) = self.receivedMessages[indexPath.row]
+        cell.textLabel?.text = s.0
+        cell.detailTextLabel?.text = s.1.description
+        return cell
     }
 
     override public func receive(msg: Message) {
@@ -23,31 +53,109 @@ public class WSRViewControler : Actor {
         case is Connect:
             let w = msg as! Connect
             wsClient ! Connect(url: w.url, delegate: this)
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(5 * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), {
-                self.wsClient ! SendMessage(sender: self.this, message: "Hi")
+            break;
+            
+        case is OnConnect:
+            NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                self.ctrl?.title = "Connected"
+                self.ctrl?.textField.becomeFirstResponder()
             })
             break;
-        case is OnConnect:
-            print("web socket is connected")
+            
+        case is SendMessage:
+            let w = msg as! SendMessage
+            wsClient ! SendMessage(sender: this, message: w.message)
             break;
             
         case is OnMessage:
             let w = msg as! OnMessage
-            print("got message \(w.message)")
+            self.receivedMessages.append((w.message, NSDate.init()))
+            NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                let i = self.receivedMessages.count - 1
+                let lastRow = NSIndexPath.init(forRow: i, inSection: 0)
+                self.ctrl?.tableView.insertRowsAtIndexPaths([lastRow], withRowAnimation: UITableViewRowAnimation.Automatic)
+                self.ctrl?.tableView.scrollToRowAtIndexPath(lastRow, atScrollPosition: UITableViewScrollPosition.Middle, animated: true)
+            })
+            break;
+            
+        case is Disconnect:
+            wsClient ! Disconnect(sender: this)
+            NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                self.ctrl?.title = "Disconnected"
+            })
+            break;
+            
+        case is SetWSController:
+            let w = msg as! SetWSController
+            ctrl = w.ctrl
+            NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                self.ctrl?.tableView.dataSource = self
+                self.ctrl?.tableView.delegate = self
+            })
             break;
             
         default:
             super.receive(msg)
         }
-        
     }
     
+    deinit {
+        self.wsClient ! Harakiri(sender: Optional.None)
+    }
 }
 
-class WSViewController : UIViewController {
-    var wsCtrl : ActorRef = AppActorSystem.shared.actorOf(WSRViewControler)
+class WSViewController : UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var sendbar: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var send: UIButton!
+    @IBOutlet weak var bottomTextField: NSLayoutConstraint!
+    var wsCtrl : ActorRef = AppActorSystem.shared.actorOf(WSRViewController)
     
     override func viewDidLoad() {
+        wsCtrl ! SetWSController(ctrl: self)
         wsCtrl ! Connect(url: NSURL(string: "wss://echo.websocket.org")!, delegate: wsCtrl)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:"keyboardWillAppear:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:"keyboardWillDisappear:", name: UIKeyboardWillHideNotification, object: nil)
+        send.addTarget(self, action: "onClick:", forControlEvents: .TouchUpInside)
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        wsCtrl ! SendMessage(sender: wsCtrl, message: textField.text!)
+        return true
+    }
+    
+    @objc func onClick(btn : UIButton) {
+        wsCtrl ! SendMessage(sender: wsCtrl, message: textField.text!)
+    }
+    
+    deinit {
+        wsCtrl ! Harakiri(sender: Optional.None)
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func keyboardWillAppear(notification: NSNotification){
+        let userInfo:NSDictionary = notification.userInfo!
+        let keyboardSize:CGSize = userInfo.objectForKey(UIKeyboardFrameBeginUserInfoKey)!.CGRectValue.size
+        
+        
+        bottomTextField.constant = keyboardSize.height;
+        self.view.setNeedsUpdateConstraints()
+        
+        UIView.animateWithDuration(0.25) { () -> Void in
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func keyboardWillDisappear(notification: NSNotification){
+        bottomTextField.constant = 0;
+        self.view.setNeedsUpdateConstraints()
+        
+        UIView.animateWithDuration(0.25) { () -> Void in
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    
 }
--- Theater/WebSocketClient.swift
+++ Theater/WebSocketClient.swift
@@ -29,6 +29,15 @@ public class OnMessage : Message {
     }
 }
 
+public class OnData : Message {
+    public let data : NSData
+    
+    public init(sender: ActorRef, data : NSData) {
+        self.data = data
+        super.init(sender: sender)
+    }
+}
+
 public class SendMessage : OnMessage {}
 
 public class Disconnect : Message {}
@@ -43,25 +52,27 @@ public class WebSocketClient : Actor , WebSocketDelegate {
     var delegate : Optional<ActorRef> = Optional.None
     
     public func websocketDidConnect(socket: WebSocket) {
-        if let delegate = self.delegate {
-            delegate ! OnConnect(sender: this)
+        if let del = self.delegate {
+            del ! OnConnect(sender: this)
         }
     }
     
     public func websocketDidDisconnect(socket: WebSocket, error: NSError?) {
-        if let delegate = self.delegate {
-            delegate ! OnDisconnect(sender: this)
+        if let del = self.delegate {
+            del ! OnDisconnect(sender: this)
         }
     }
     
     public func websocketDidReceiveMessage(socket: WebSocket, text: String) {
-        if let delegate = self.delegate {
-            delegate ! OnMessage(sender: this, message: text)
+        if let del = self.delegate {
+            del ! OnMessage(sender: this, message: text)
         }
     }
     
     public func websocketDidReceiveData(socket: WebSocket, data: NSData) {
-        
+        if let del = self.delegate {
+            del ! OnData(sender: this, data: data)
+        }
     }
     
     override public func receive(msg: Message) {
@@ -78,6 +89,8 @@ public class WebSocketClient : Actor , WebSocketDelegate {
             case is Disconnect:
                 if let s = self.socket {
                     s.disconnect()
+                    self.socket!.delegate = nil
+                    self.socket = nil
                 }
                 break;
             
@@ -88,6 +101,10 @@ public class WebSocketClient : Actor , WebSocketDelegate {
                 self.delegate = c.sender
                 self.socket!.connect()
             break;
+            
+            case is Harakiri:
+                this ! Disconnect(sender: Optional.None)
+                break;
             
             default:
                 super.receive(msg)
