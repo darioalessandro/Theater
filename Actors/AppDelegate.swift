c617fac4c7f0dee26f8b41a85161f2e2a58e98bd
darioalessandro <darioalessandrolencina@gmail.com>
2015-10-10 20:38:21 -0400

    configured lumberjack correctly

--- Actors/AppDelegate.swift
+++ Actors/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Theater
+import CocoaLumberjack
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        defaultDebugLevel = DDLogLevel.Debug
+        DDLog.addLogger(DDTTYLogger.sharedInstance())
         return true
     }
 
