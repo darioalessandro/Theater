b8d2d269a78fdda364598bf2cb5b1da41e1aaac4
darioalessandro <darioalessandrolencina@gmail.com>
2015-10-26 22:30:46 -0400

    missing files

---
+++ Classes/WithListeners.swift
@@ -0,0 +1,42 @@
+//
+//  WithListeners.swift
+//  Actors
+//
+//  Created by Dario Lencina on 10/24/15.
+//  Copyright © 2015 dario. All rights reserved.
+//
+
+import Foundation
+
+public protocol WithListeners : class {
+    
+    var listeners : [ActorRef] { get set }
+    
+    func addListener(sender : Optional<ActorRef>)
+    
+    func removeListener(sender : Optional<ActorRef>)
+    
+    func broadcast(msg : Message)
+
+}
+
+extension WithListeners {
+        
+    public func addListener(sender : Optional<ActorRef>) {
+        if let s = sender {
+            
+            if (listeners.contains({ a -> Bool in return s.path.asString == a.path.asString}) == false) {
+                listeners.append(s)
+            }
+        }
+    }
+    
+    public func removeListener(sender : Optional<ActorRef>) {
+        if let l = sender,
+            n = listeners.indexOf({ a -> Bool in  return l.path.asString == a.path.asString}) {
+                listeners.removeFirst(n)
+        }
+    }
+    
+    public func broadcast(m : Message) { listeners.forEach { $0 ! m} }
+}
