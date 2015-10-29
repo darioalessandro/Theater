d9b15d8b7469c0b7f6ad44f01b382b817461977f
darioalessandro <darioalessandrolencina@gmail.com>
2015-10-28 20:35:49 -0400

    Removed useless test class

--- TheaterTests/TheaterTests.swift
+++ TheaterTests/TheaterTests.swift
@@ -10,6 +10,48 @@ import XCTest
 import Theater
 @testable import Theater
 
+class TheaterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testTestSending10Msg() {
+        let expectation = expectationWithDescription("Swift Expectations")
+        let system = AppActorSystem.shared
+        
+        let ping = system.actorOf(TestActor)
+        let pong = system.actorOf(TestActor)
+        
+        pong ! TestPerformance(sender: ping, count: 0, max: 10, expectation: expectation)
+        
+        self.waitForExpectationsWithTimeout(10) { (error) -> Void in
+            print("error \(error)")
+        }
+    }
+    
+    
+    func testThatDeadLettersAreGeneratedWhenTryingToReachADeathActor() {
+        let expectation = expectationWithDescription("Swift Expectations")
+        let system = AppActorSystem.shared
+        
+        let medium = system.actorOf(ChrisRedfield.self, name: "Medium")
+        let zombie = system.actorOf(Actor.self, name: "Zombie")
+        
+        medium ! ChrisRedfield.SetExpectation(expectation: expectation)
+        system.stop(zombie)
+        zombie ! Message(sender: medium)
+        
+        self.waitForExpectationsWithTimeout(2) { (error) -> Void in
+            print("error \(error)")
+        }
+    }
+}
+
 public class ChrisRedfield : Actor {
     
     public required init(context: ActorSystem, ref: ActorRef) {
@@ -40,47 +82,4 @@ public class ChrisRedfield : Actor {
             XCTAssert(false)
         }
     }
-}
-
-class TheaterTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-    }
-    
-    func testTestSending2000Msg() {
-        let expectation = expectationWithDescription("Swift Expectations")
-        let system = AppActorSystem.shared
-        
-        let ping = system.actorOf(TestActor)
-        let pong = system.actorOf(TestActor)
-        
-        pong ! TestPerformance(sender: ping, count: 0, max: 10, expectation: expectation)
-        
-        self.waitForExpectationsWithTimeout(10) { (error) -> Void in
-            print("error \(error)")
-        }
-    }
-    
-    
-    func testThatDeadLettersAreGeneratedWhenTryingToReachADeathActor() {
-        let expectation = expectationWithDescription("Swift Expectations")
-        let system = AppActorSystem.shared
-        
-        let medium = system.actorOf(ChrisRedfield.self, name: "Medium")
-        let zombie = system.actorOf(Actor.self, name: "Zombie")
-        
-        medium ! ChrisRedfield.SetExpectation(expectation: expectation)
-        system.stop(zombie)
-        zombie ! Message(sender: medium)
-        
-        self.waitForExpectationsWithTimeout(2) { (error) -> Void in
-            print("error \(error)")
-        }
-    }
-    
 }
