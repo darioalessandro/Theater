72281343b139666ce9ddfe38bdf7dafd2707235f
darioalessandro <darioalessandrolencina@gmail.com>
2015-12-21 20:49:21 -0500

    got master to compile

--- Actors/WSViewController.swift
+++ Actors/WSViewController.swift
@@ -167,7 +167,7 @@ class WSViewController : UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         wsCtrl ! SetViewCtrl(ctrl: self)
-        wsCtrl ! WebSocketClient.Connect(url: NSURL(string: "wss://echo.websocket.org")!, sender : nil)
+        wsCtrl ! WebSocketClient.Connect(url: NSURL(string: "http://localhost:9000")!, sender : nil)
         self.addNotifications()
         send.addTarget(self, action: "onClick:", forControlEvents: .TouchUpInside)
     }
