a2d74b65bd665c3f36b5f316e03d95cdb2aaf46d
Dario Talarico <darioalessandrolencina@gmail.com>
2016-08-06 22:50:30 -0400

    Added ActorsDemo for test cases

---
+++ TheaterTests/ActorsDemo.swift
@@ -0,0 +1,46 @@
+//
+//  ActorsDemo.swift
+//  Actors
+//
+//  Created by Dario Lencina on 9/26/15.
+//  Copyright © 2015 dario. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Theater
+
+public class TestPerformance : Actor.Message {
+    public let count : NSInteger
+    public let max : NSInteger
+    public let expectation : XCTestExpectation
+
+    
+    public init(sender : ActorRef, count : NSInteger, max : NSInteger, expectation : XCTestExpectation) {
+        self.count = count
+        self.max = max
+        self.expectation = expectation
+        super.init(sender : sender)
+    }
+}
+
+public class TestActor : Actor {
+    override public func receive(msg : Actor.Message) -> Void {
+        switch msg {
+        case is TestPerformance:
+                let test = msg as! TestPerformance
+                print("got message in TestActor")
+                if test.count > test.max {
+                    print("The end")
+                    test.expectation.fulfill()
+                } else {
+                    if let sender = self.sender {
+                        sender ! TestPerformance(sender: this, count: test.count + 1, max: test.max, expectation: test.expectation)
+                    }
+                }
+                break
+            default :
+            print("I do not know what you're talking about")
+        }
+    }
+}
